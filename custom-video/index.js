88990626bde9a4a0f56b31cf668b720a17f29505
Anna Kalenik <annakalenikalex@gmail.com>
2022-02-05 16:09:19 +0300

    feat: add video control

--- custom-video/index.js
+++ custom-video/index.js
@@ -5,11 +5,9 @@ const btnBigPlay = document.querySelector('.player-big-button-play');
 const btnPlay = document.querySelector('.player-button-play');
 const btnSound = document.querySelector('.player-button-sound');
 
-const inputPlay = document.querySelector('[name="playback"]');
+const playerLoading = document.querySelector('.player-loading');
+const playerLoadingBar = document.querySelector('.player-loading-bar');
 const inputSound = document.querySelector('[name="volume"]');
-const arrProgressInputs = document.querySelectorAll('.player-progress');
-
-let progression;
 
 video.addEventListener('click', toggleVideo);
 btnBigPlay.addEventListener('click', toggleVideo);
@@ -17,30 +15,34 @@ btnPlay.addEventListener('click', toggleVideo);
 function toggleVideo() {
     if (video.paused) {
         video.play();
-        // updateCurrentPosVideo();
-        // progression = window.setInterval(inputPlay.addEventListener('input', updateProgressVideo),200);
         btnPlay.classList.add('toggle');
         btnBigPlay.classList.add('transparent');
     } else {
         video.pause();
-        // clearInterval(progression);
         btnPlay.classList.remove('toggle');
         btnBigPlay.classList.remove('transparent');
     }
 }
 
-// inputPlay.addEventListener('change', updateCurrentPosVideo);
-// function updateCurrentPosVideo() {
-//     const value = inputPlay.value;
-//     video.currentTime = value * video.duration;
-// }
+video.addEventListener('timeupdate', updatelLoading)
+function updatelLoading() {
+    const percent = (video.currentTime / video.duration) * 100;
+    playerLoadingBar.style.flexBasis = `${percent}%`;
+}
+
+playerLoading.addEventListener('click', handleLoading);
+function handleLoading(e) {
+    const handleTime = (e.offsetX / playerLoading.offsetWidth) * video.duration;
+    console.log(e.offsetX);
+    video.currentTime = handleTime;
+}
 
 video.addEventListener('ended', isVideoEnded);
 function isVideoEnded() {
     if (video.ended) {
         btnPlay.classList.remove('toggle');
         btnBigPlay.classList.remove('transparent');
-        inputPlay.value = '0';
+        playerLoadingBar.style.flexBasis = '0%';
     }
 }
 
@@ -61,8 +63,8 @@ function isVolDisable() {
     inputSound.value === '0' ? btnSound.classList.add('toggle') : btnSound.classList.remove('toggle');
 }
 
-arrProgressInputs.forEach((input) => input.addEventListener('input', updateProgress));
+inputSound.addEventListener('input', updateProgress);
 function updateProgress() {
     const value = this.value;
-    this.style.background = `linear-gradient(to right, #d6eee2 0%, #c2f0da ${value * 100}%, #fff ${value * 100}%, #fff 100%)`;
+    this.style.background = `linear-gradient(to right, #d6eee2 0%, #c2f0da ${value * 100}%, rgba(255, 255, 255, 0.4) ${value * 100}%, rgba(255, 255, 255, 0.4) 100%)`;
 }
